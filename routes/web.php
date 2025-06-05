501bac6c7cfe61d4421fbbf9ba2435d4f93c2eea
Kauan Calheiro <110562396+KauanCalheiro@users.noreply.github.com>
2025-06-05 19:45:37 -0300

    V5 (#14)
    * teste
    
    * V1 (#7) (#8)
    
    * test: success (#5)
    
    * feat: atualiza o projeto
    
    * feat: removing problematic test
    
    * teste
    
    * v2 (#10)
    
    * V1 (#7)
    
    * test: success (#5)
    
    * feat: atualiza o projeto
    
    * feat: removing problematic test
    
    * teste
    
    * V2 (#9)
    
    * teste
    
    * V1 (#7) (#8)
    
    * test: success (#5)
    
    * feat: atualiza o projeto
    
    * feat: removing problematic test
    
    * teste
    
    * V2
    
    * feat: update test
    
    * V4 (#13)
    
    * teste
    
    * feat: update test
    
    * v4
    
    * refactor: remove unused task test methods
    
    * refactor: remove unused task test methods
    
    * feat: adicionar funcionalidades de gerenciamento de tarefas
    
    * refactor: remove redundant test for accessing protected routes with login

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use App\Livewire\Settings\Appearance;
 use App\Livewire\Settings\Password;
 use App\Livewire\Settings\Profile;
+use App\Models\Task;
+use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -19,6 +22,17 @@ Route::middleware(['auth'])->group(function () {
     Route::get('settings/profile', Profile::class)->name('settings.profile');
     Route::get('settings/password', Password::class)->name('settings.password');
     Route::get('settings/appearance', Appearance::class)->name('settings.appearance');
+
+    Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
+    Route::get('/tasks/create', [TaskController::class, 'create'])->name('tasks.create');
+    Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
+    Route::get('/tasks/{task}/edit', [TaskController::class, 'edit'])->name('tasks.edit');
+    Route::put('/tasks/{task}', [TaskController::class, 'update'])->name('tasks.update');
+    Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
+    Route::get('/tasks/{task}/pdf', function ($task) {
+        $pdf = Pdf::loadView('pdf_task', ['task' => Task::findOrFail($task)]);
+        return $pdf->download('task.pdf');
+    })->name('tasks.pdf');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
